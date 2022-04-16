659f0a19235b873108aa1f6ffdbbf8c368cdbf4b
Raul Alonzo <ram2489@gmail.com>
2022-04-16 14:38:58 -0500

    FEAT: Finished Write Your First Flutter App ,pt 1
    App creates infinite ListRow widgets with a random word
    App integrates the third party library english_words
    App splits main from random_words widget

---
+++ dart-flutter-challenges/flutter/flutter-material/00-flutter-first-app/startup_namer/lib/random_words.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
+
+class RandomWords extends StatefulWidget {
+  const RandomWords({Key? key}) : super(key: key);
+
+  @override
+  State<RandomWords> createState() => _RandomWordsState();
+}
+
+class _RandomWordsState extends State<RandomWords> {
+  final _suggestions = <WordPair>[];
+  final _biggerFont = const TextStyle(fontSize: 18);
+
+  Widget _buildSuggestions() {
+    return ListView.builder(
+        padding: const EdgeInsets.all(16),
+        itemBuilder: (context, idx) {
+          if (idx.isOdd) {
+            return const Divider();
+          }
+          final index = idx ~/2;
+          if (index >= _suggestions.length) {
+            _suggestions.addAll(generateWordPairs().take(10));
+          }
+          return _buildRow(_suggestions[index]);
+        },
+    );
+  }
+
+  Widget _buildRow(WordPair pair) {
+    return ListTile(
+      title: Text(
+        pair.asPascalCase,
+        style: _biggerFont,
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final wordPair = WordPair.random();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Startup Name Generator'),
+      ),
+      body: _buildSuggestions(),
+    );
+  }
+}
