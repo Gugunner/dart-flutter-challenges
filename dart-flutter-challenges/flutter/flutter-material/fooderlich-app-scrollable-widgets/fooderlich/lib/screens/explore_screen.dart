e60accaed2cd53d74b73625d54bfc789d8cb1554
Raul Alonzo <ram2489@gmail.com>
2022-04-18 18:10:29 -0500

    FEAT: Added a ScrollController listener to check scroll offset

--- dart-flutter-challenges/flutter/flutter-material/fooderlich-app-scrollable-widgets/fooderlich/lib/screens/explore_screen.dart
+++ dart-flutter-challenges/flutter/flutter-material/fooderlich-app-scrollable-widgets/fooderlich/lib/screens/explore_screen.dart
@@ -4,9 +4,39 @@ import '../components/components.dart';
 import '../models/models.dart';
 import '../api/mock_fooderlich_service.dart';
 
-class ExploreScreen extends StatelessWidget {
+class ExploreScreen extends StatefulWidget {
+
+  const ExploreScreen({Key? key}) : super(key: key);
+
+  @override
+  State<ExploreScreen> createState() => _ExploreScreenState();
+}
+
+class _ExploreScreenState extends State<ExploreScreen> {
   final mockService = MockFooderlichService();
-  ExploreScreen({Key? key}) : super(key: key);
+  late ScrollController _controller;
+
+  void _scrollListener() {
+    if (_controller.offset >= _controller.position.maxScrollExtent && !_controller.position.outOfRange) {
+      debugPrint('I\'m at the bottom!');
+    }
+    if (_controller.offset <= _controller.position.minScrollExtent && !_controller.position.outOfRange) {
+      debugPrint('I\'m at the top!');
+    }
+  }
+
+  @override
+  void initState() {
+    _controller = ScrollController();
+    _controller.addListener(_scrollListener);
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _controller.removeListener(_scrollListener);
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -15,6 +45,7 @@ class ExploreScreen extends StatelessWidget {
         builder: (context, AsyncSnapshot<ExploreData> snapshot) {
           if (snapshot.connectionState == ConnectionState.done) {
             return ListView(
+              controller: _controller,
               scrollDirection: Axis.vertical,
               children: [
                 TodayRecipeListView(recipes: snapshot.data?.todayRecipes ?? []),
