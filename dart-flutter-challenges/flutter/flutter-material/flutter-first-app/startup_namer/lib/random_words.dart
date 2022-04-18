fd70d4da1c342f9a2a1a807764731c0fe8ee87be
Raul Alonzo <ram2489@gmail.com>
2022-04-18 12:36:36 -0500

    FEAT: Added second part of flutter tutorial
    Added a clickable heart icon as a favorite that toggles
    Added a funcionality to store in a set different word pairs 
    Added a new navigation route for another window to showcase favorite word pairs

--- dart-flutter-challenges/flutter/flutter-material/flutter-first-app/startup_namer/lib/random_words.dart
+++ dart-flutter-challenges/flutter/flutter-material/flutter-first-app/startup_namer/lib/random_words.dart
@@ -10,7 +10,55 @@ class RandomWords extends StatefulWidget {
 
 class _RandomWordsState extends State<RandomWords> {
   final _suggestions = <WordPair>[];
+  final _saved = <WordPair>{};
   final _biggerFont = const TextStyle(fontSize: 18);
+
+  void _pushSaved() {
+    Navigator.of(context).push(
+      MaterialPageRoute<void>(
+        builder: (context) {
+          final tiles = _saved.map(
+              (pair) {
+                return ListTile(
+                  title: Text(
+                    pair.asPascalCase,
+                    style: _biggerFont,
+                  ),
+                );
+              },
+          );
+          final divided = tiles.isNotEmpty ? ListTile.divideTiles(
+            context: context,
+            tiles: tiles,
+          ).toList() : <Widget>[];
+          return Scaffold(
+            appBar: AppBar(
+              title: const Text('Saved Suggestions'),
+            ),
+            body: ListView(children: divided),
+          );
+        }
+      )
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final wordPair = WordPair.random();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Startup Name Generator'),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.list),
+            onPressed: _pushSaved,
+            tooltip: 'Saved Suggestions',
+          )
+        ],
+      ),
+      body: _buildSuggestions(),
+    );
+  }
 
   Widget _buildSuggestions() {
     return ListView.builder(
@@ -29,22 +77,31 @@ class _RandomWordsState extends State<RandomWords> {
   }
 
   Widget _buildRow(WordPair pair) {
+    final alreadySaved = _saved.contains(pair);
     return ListTile(
       title: Text(
         pair.asPascalCase,
         style: _biggerFont,
       ),
+      trailing: Icon(
+        alreadySaved ? Icons.favorite : Icons.favorite_border,
+        color: alreadySaved ? Colors.red : null,
+        semanticLabel: alreadySaved ? 'Removed from saved' : 'Save',
+      ),
+      onTap: () {
+        debugPrint('TAP');
+        setState(() {
+          if (alreadySaved) {
+            debugPrint('YES');
+            _saved.remove(pair);
+          } else {
+            debugPrint('NO');
+            _saved.add(pair);
+          }
+        });
+      },
     );
   }
 
-  @override
-  Widget build(BuildContext context) {
-    final wordPair = WordPair.random();
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Startup Name Generator'),
-      ),
-      body: _buildSuggestions(),
-    );
-  }
+
 }
